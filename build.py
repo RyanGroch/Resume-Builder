de5c62cf69d676208eecc0301826c45b9569f8c6
Ryan Groch <ryan.w.groch@gmail.com>
2025-06-17 17:19:29 -0500

    Large refactor; cleans up a lot of build.py and changes the way that nested bullet points work

--- build.py
+++ build.py
@@ -17,18 +17,43 @@ EDU_TEMPL = Path("templates/education-template.txt").read_text()
 
 IMPORT_TOKEN = "\input{{resume/{name}.tex}}\n"
 BOLD_ITEM_TOKEN = "\\textbf{{{skill_category}}}: {content}"
-BOLD_ITEM_NEWLINE_TOKEN = f"{BOLD_ITEM_TOKEN} \\\\\n"
 BULLET_ITEM_TOKEN = "\\item {{{item}}}"
-INDENTED_BULLET_ITEM_TOKEN = "\hspace{{1mm}} \\bullet \hspace{{1mm}} {item} \\newline"
+INDENTED_BULLET_ITEM_TOKEN = "\hspace{{{space}}} \\bullet \hspace{{1mm}} {item}"
+LINE_BREAK_TOKEN = " \\\\ \n"
+NEWLINE_TOKEN = " \\newline\n" + 8 * " "
 
-edu_std_point = "        \item {{{0}}}"
-edu_custom_1 = "         {0} \\newline"
-edu_custom_2 = "         \hspace{{1mm}} \\bullet \hspace{{1mm}} {0} \\newline"
-edu_custom_3 = "         \hspace{{5mm}} \\bullet \hspace{{1mm}} {0} \\newline"
+INDENT_HIERARCHY = [
+    None,
+    "1mm",
+    "5mm",
+    "9mm",
+    "13mm"
+]
 
 # Section types
 PARAGRAPH = "cvparagraph"
 ENTRIES = "cventries"
+
+
+def render_indented_points(point, indent_level, exclude):
+    if isinstance(point, dict):
+        return NEWLINE_TOKEN.join([
+            render_indented_points(
+                value, 
+                indent_level + 1, 
+                exclude
+            )
+            for key, value in point.items()
+            if key not in exclude
+        ])
+    
+    if INDENT_HIERARCHY[indent_level] is None:
+        return BULLET_ITEM_TOKEN.format(item=point)
+    
+    return INDENTED_BULLET_ITEM_TOKEN.format(
+        item=point,
+        space=INDENT_HIERARCHY[indent_level]
+    )   
 
 
 def build_summary(recipe):
@@ -36,117 +61,86 @@ def build_summary(recipe):
 
 
 def build_skills(recipe):
-    skills_str = ""
-    for i, section in enumerate(recipe["skills"]):
-        curr_skill_str = ""
-
-        for j, skill in enumerate(section["content"]):
-            if j != 0:
-                curr_skill_str += ", "
-
-            curr_skill_str += DATA["skills"][skill]
-
-        token = BOLD_ITEM_NEWLINE_TOKEN \
-            if i != len(recipe["skills"]) - 1 \
-            else BOLD_ITEM_TOKEN
-
-        skills_str += token.format(
-            skill_category=section["name"],
-            content=curr_skill_str
+    return LINE_BREAK_TOKEN.join(
+        map(lambda section: 
+            BOLD_ITEM_TOKEN.format(
+                skill_category=section["name"],
+                content=", ".join(
+                    [DATA["skills"][skill] for skill in section["content"]]
+                )
+            ),
+            recipe["skills"]
         )
-
-    return skills_str
+    )
 
 
 def build_projects(recipe):
-    projects_str = ""
+    projects = []
         
     for project_recipe in recipe["projects"]:
         project = DATA["projects"][project_recipe['name']]
-        project_items_str = ""
+        points = render_indented_points(project["points"], -1, project_recipe["exclude"])
 
         if project["tech"]:
-            project_items_str += BULLET_ITEM_TOKEN.format(
+            tech = BULLET_ITEM_TOKEN.format(
                 item=BOLD_ITEM_TOKEN.format(
                     skill_category="Technologies:",
                     content=project["tech"]
                 )
-            ) + "\n" + 8 * " "
+            )
+            points = tech + NEWLINE_TOKEN + points
 
-        for i, (key, value) in enumerate(project["points"].items()):
-            if key not in project_recipe["exclude"]:
-                curr_proj_str = BULLET_ITEM_TOKEN.format(item=value)
-
-                if i < len(project["points"]) - 1:
-                    curr_proj_str += "\n"
-                
-                project_items_str += curr_proj_str + 8 * " "
-
-        projects_str += PROJECT_TEMPL.format(
-            href=project["href"],
-            link=project["link"],
-            name=project["name"],
-            date=project["date"],
-            content=(project_items_str)
+        projects.append(
+            PROJECT_TEMPL.format(
+                href=project["href"],
+                link=project["link"],
+                name=project["name"],
+                date=project["date"],
+                content=points
+            )
         )
 
-    return projects_str
+    return "".join(projects)
 
 
 def build_experience(recipe):
-    exp_str = ""
+    experiences = []
     for experience_recipe in recipe["experience"]:
         experience = DATA["experience"][experience_recipe["name"]]
-        curr_exp_str = ""
-        
-        for key, value in experience["points"].items():
-            if key not in experience_recipe["exclude"]:
-                if isinstance(value, dict):
-                    for subval in value.values():
-                        curr_exp_str += INDENTED_BULLET_ITEM_TOKEN.format(item=subval)
-                else:
-                    curr_exp_str += BULLET_ITEM_TOKEN.format(item=value) + "\\newline"
 
-        exp_str += EXP_TEMPL.format(
+        points_str = render_indented_points(
+            experience["points"],
+            -1,
+            experience_recipe["exclude"]
+        )
+
+        experiences.append(EXP_TEMPL.format(
             name=experience["name"],
             employer=experience["employer"],
             date=experience["date"],
-            content=curr_exp_str
-        )
+            content=points_str
+        ))
 
-    return exp_str
+    return "".join(experiences)
 
 
 def build_education(recipe):
-    education_str = ""
+    educations = []
     for education_recipe in recipe["education"]:
         education = DATA["education"][education_recipe["name"]]
 
-        points_str = ""
-        for key, value in education["points"].items():
-            if education["notation"] == "standard":
-                points_str += edu_std_point.format(value)
-            elif not isinstance(value, dict):
-                if key == "main":
-                    points_str += edu_custom_1.format(value)
-
-                points_str += edu_custom_2.format(value)
-            else:
-                for subkey, subvalue in value.items():
-                    if subkey == "main":
-                        points_str += edu_custom_2.format(subvalue)
-                    else:
-                        points_str += edu_custom_3.format(subvalue)
-
-        education_str += EDU_TEMPL.format(
+        educations.append(EDU_TEMPL.format(
             degree=education["degree"],
             institution=education["institution"],
             location=education["location"],
             date=education["date"],
-            content=points_str
-        )
+            content=render_indented_points(
+                education["points"],
+                -1, 
+                education_recipe["exclude"])
+        ))
 
-    return education_str
+    return "".join(educations)
 
 
 SECTIONS = [
@@ -157,13 +151,13 @@ SECTIONS = [
     ("education", "Education", ENTRIES, build_education)
 ]
 
+
 def main():
     if len(sys.argv) < 2:
         print("Usage: python3 build.py recipe")
         return
     
     recipe_name = sys.argv[1]
-
     recipe = RECIPES[recipe_name]
 
     shutil.rmtree("build", ignore_errors=True)
@@ -180,15 +174,17 @@ def main():
         out_file.write(RESUME_TEMPL.format(content=content))
 
     for section in SECTIONS:
-        content = section[3](recipe)
-        with open(f"build/resume/{section[0]}.tex", "w") as out_file:
-            out_file.write(SECTION_TEMPL.format(
-                section_name=section[1],
-                section_type=section[2],
-                content=content
-            ))
+        content = SECTION_TEMPL.format(
+            section_name=section[1],
+            section_type=section[2],
+            content=section[3](recipe)
+        )
+
+        with open(f"build/resume/{section[0]}.tex", "w") as f: 
+            f.write(content)
 
     subprocess.run(["xelatex", "resume.tex"], cwd="./build")
+
 
 if __name__ == "__main__":
     main()
